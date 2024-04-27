66ed4c46423574d8fc60bac053fcdad9b9e76182
phoenixsteve0012 <phoenixsparrow00700@gmail.com>
2024-04-27 16:33:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -479,10 +479,6 @@ Size: **{data["size"]}**
             int(count) + 1 if count else 1,
             ex=7200,
         )
- app = web.AppRunner(await web_server()
- await app.setup()
- bind_address = "0.0.0.0"
- await web.TCPSite(app, bind_address, PORT).start()
 
 bot.start(bot_token=BOT_TOKEN)
 print("Bot started!")
