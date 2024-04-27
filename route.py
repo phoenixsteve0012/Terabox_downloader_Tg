dd4e48e400f7c761b40152206b3e8eef7f0e2cf4
phoenixsteve0012 <phoenixsparrow00700@gmail.com>
2024-04-27 16:36:35 +0530

    Update route.py

--- route.py
+++ route.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from flask import Flask
 
 routes = web.RouteTableDef()
 
@@ -11,3 +12,14 @@ async def web_server():
     web_app = web.Application(client_max_size=90000000)
     web_app.add_routes(routes)
     return web_app
+
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 4000))
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(port=port)
+
